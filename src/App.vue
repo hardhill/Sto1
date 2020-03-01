cd2ae0535b7ea3392dcadd011eb4d8562373a790
Waclaw Bielecki <hardhill@mail.ru>
2020-03-01 23:06:20 +0800

    fist

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <div class="header_game">СТО К ОДНОМУ</div>
     <router-view/>
   </div>
 </template>
 
 <style>
+*{
+  margin:0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  width: 100%;
+  height: 100vh;
+  background-color: #3d4e86;
+  display: flex;
+  justify-content: center;
+}
+.header_game{
+  color: yellow;
+  margin: 2rem 1rem;
+  font-size: 2rem;
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="game_one">
+        sasdasdasda
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
+<style scoped>
+.game_one{
+  display: flex;
+  border: 1px solid #666;
+}
+</style>
